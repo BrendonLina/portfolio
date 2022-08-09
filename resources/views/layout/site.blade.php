80078d26ee670c0ef33fef62d2b4e25a82154946
Brendon Lina <avgwest@outlook.com>
2022-08-09 00:29:43 -0300

    construcao do nav-menu

---
+++ resources/views/layout/site.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Portfolio</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <header>
+        <div class="menu">
+            <div class="menu-top">  
+                <ul>
+                    <li><a href="{{ url('/') }}">Home</a></li>
+                    <li><a href="{{ url('/projetos') }}">Meus Projetos</a></li>
+                    <li><a href="{{ url('/clientes') }}">Meus clientes</a></li>
+                    <li><a href="{{ url('/contato') }}">Contato</a></li>
+                </ul>
+            </div>
+        </div>
+
+    </header>
+    <section>
+            <h1>ABCD</h1>
+            <h2>ABCD</h2>
+            <h3>ABCD</h3>
+            <h4>ABCD</h4>
+    </section>
+    <footer>
+        <h6>footer</h6>
+    </footer>
+
+    <script type="text/javascript" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js">
+
+    </script>
+</body>
+</html>
