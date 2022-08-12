64b086bf5d45502de08bbf94faa1380d216d2d09
Brendon Lina <avgwest@outlook.com>
2022-08-11 22:33:12 -0300

    menu e header

--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -4,27 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Meu portfólio !">
+    <meta name="keywords" content="portfolio, curriculo, agencia">
     <title>Meu Portfolio</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <header class="header">  
-            <div>Brendon Lina</div>
-    </header>
-
-    <nav class="navbar">
-        <a href="#">Home</a>
-        <a href="#">Meus Projetos</a>
-        <a href="#">Meus Clientes</a>
-        <a href="#">Contato</a>
-    </nav>
-    <section>
-        
-    </section>
-    <footer>
-        <h6>footer</h6>
-    </footer>
+   <nav class="navbar">
+        <div class="logo">
+            <h1>Brendon Lina</h1>
+        </div>
+        <div class="menu-top">
+            <a href="#">Home</a>
+            <a href="#">Meus Projetos</a>
+            <a href="#">Porfólio</a>
+            <a href="#" id="botao">Contato</a>
+        </div>
+   </nav>
+   <header class="header">
+       <div class="headline">
+           <h2 id="bem-vindo">Bem vindo ao meu site!</h2>
+           <h2 id="portfolio">Esse é meu portfólio</h2>
+           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem eum aspernatur pariatur cupiditate totam obcaecati ut ullam, necessitatibus dolorum. Nihil quod repellendus quos quidem magni beatae quo quibusdam maiores sint!</p>
+           <a href="#" class="contact-btn">FALE COMIGO</a>
+       </div>
+       <div class="img-headline">
+           <img src="/img/3964906.jpg" alt="programador" style="max-width:100%;">
+       </div> 
+   </header>
+   <section>
+       <h2>O que podemos fazer por você?</h2>
+   </section>
 
     <script type="text/javascript" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js">
 
