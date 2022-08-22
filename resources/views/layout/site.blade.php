601b608a2a4016fda0dfd370c2a126325c979fbc
Brendon Lina <avgwest@outlook.com>
2022-08-22 14:02:19 -0300

    alt campos comentados

--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -218,15 +218,12 @@
             @csrf
             <h1 class="mensagem">Deixe um comentário!</h1>
             <div class="mb-3">
-                <!-- <label for="exampleFormControlInput1" class="form-label">Nome</label> -->
                 <input type="text" class="form-control" id="validationCustomUsername" name="name" placeholder="Seu nome" required>
             </div>
             <div class="mb-3">
-                <!-- <label for="exampleFormControlInput1" class="form-label">Email</label> -->
                 <input type="email" class="form-control" id="exampleFormControlInput1" name="email" placeholder="Seu email" required>
             </div>
             <div class="mb-3">
-                <!-- <label for="exampleFormControlTextarea1" class="form-label">Feedback / Mensagem</label> -->
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="comentario" placeholder="Seu comentário" required></textarea>
             </div>
             <div class="mb-3">
